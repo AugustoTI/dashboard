61c6949498955cb78aa401d59ea53d52400b2a37
AugustoTI <augustocesarti2021@gmail.com>
2022-10-02 15:18:48 -0300

    creating the dasboard 'Header' component

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import { Header } from '../components/Header'
+
+const Dashboard: NextPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default Dashboard
