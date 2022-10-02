4a8b91437b431f5d22ab0a4bc10f443ed645e3dd
AugustoTI <augustocesarti2021@gmail.com>
2022-10-02 16:33:34 -0300

    creating the 'Sidebar' component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,16 @@
+import { Flex } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
 
 const Dashboard: NextPage = () => {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Header />
-    </>
+      <Flex w="100%" maxW={1400} mx="auto" my="6" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
 
