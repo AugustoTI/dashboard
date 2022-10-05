8f956f7b9772228aa52d4ceaa9f66db2d27734e1
AugustoTI <augustocesarti2021@gmail.com>
2022-10-04 23:31:48 -0300

    adding responsiveness in 'Sidebar' component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
 import { Logo } from './Logo'
 import { SearchBox } from './SearchBox'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext'
 
 export const Header = () => {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,16 @@ export const Header = () => {
       px="6"
       alignItems="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          mr="2"
+          onClick={onOpen}
+        ></IconButton>
+      )}
       <Logo />
       {isWideVersion && <SearchBox />}
       <Flex ml="auto" alignItems="center">
