78d91fb9372416c74bb9cbd4596fef66b7250b2e
AugustoTI <augustocesarti2021@gmail.com>
2022-10-06 13:16:16 -0300

    validating form with 'react-hook-form' and 'yup'

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -3,16 +3,23 @@ import {
   FormLabel,
   InputProps as ChakraInputProps,
   Input as ChakraInput,
+  FormErrorMessage,
 } from '@chakra-ui/react'
+import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { FieldError } from 'react-hook-form'
 
 interface InputProps extends ChakraInputProps {
   name: string
+  error?: FieldError
   label?: string
 }
 
-export const Input = ({ label, name, ...rest }: InputProps) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { label, name, error, ...rest },
+  ref,
+) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         name={name}
@@ -24,8 +31,12 @@ export const Input = ({ label, name, ...rest }: InputProps) => {
         _hover={{
           bgColor: 'gray.900',
         }}
+        ref={ref}
         {...rest}
       />
+      {error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   )
 }
+
+export const Input = forwardRef(InputBase)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,39 @@
 import { Button, Flex, Stack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import { Input } from '../components/Form/Input'
 
+interface HomeFormFields {
+  email: string
+  password: string
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('Email é obrigatório').email('Preencha um Email válido'),
+  password: yup.string().required('Senha é obrigatória'),
+})
+
 const Home: NextPage = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<HomeFormFields>({
+    resolver: yupResolver(signInFormSchema),
+  })
+
+  const onSubmit: SubmitHandler<HomeFormFields> = async dataFields => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(dataFields)
+  }
+
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex
+        onSubmit={handleSubmit(onSubmit)}
         as="form"
         width="100%"
         maxW={400}
@@ -15,10 +43,26 @@ const Home: NextPage = () => {
         flexDirection="column"
       >
         <Stack spacing={4}>
-          <Input type="email" name="email" label="E-Mail" />
-          <Input type="password" name="password" label="Senha" />
+          <Input
+            type="email"
+            label="E-Mail"
+            error={errors.email}
+            {...register('email')}
+          />
+          <Input
+            type="password"
+            label="Senha"
+            error={errors.password}
+            {...register('password')}
+          />
         </Stack>
-        <Button type="submit" mt="6" size="lg" colorScheme="pink">
+        <Button
+          type="submit"
+          mt="6"
+          size="lg"
+          colorScheme="pink"
+          isLoading={isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
