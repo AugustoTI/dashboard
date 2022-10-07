43246a03781e790f6f1218ddd5ddd6e85ea6336b
AugustoTI <augustocesarti2021@gmail.com>
2022-10-07 12:09:16 -0300

    serving false initial data in miragejs

--- src/services/miraje.ts
+++ src/services/miraje.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import { faker } from '@faker-js/faker'
 
 interface User {
   name: string
@@ -10,6 +11,22 @@ export const makeServer = () => {
   const server = createServer({
     models: {
       user: Model.extend<Partial<User>>({}),
+    },
+    factories: {
+      user: Factory.extend({
+        name(index) {
+          return `User ${index + 1}`
+        },
+        email() {
+          return faker.internet.email().toLocaleLowerCase()
+        },
+        createdAt() {
+          return faker.date.recent(10)
+        },
+      }),
+    },
+    seeds(server) {
+      server.createList('user', 25)
     },
     routes() {
       this.namespace = 'api'
